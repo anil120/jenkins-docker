5dbca279bf8d00cc378e144d1fe3c44ac91957af
anil120 <46101509+anil120@users.noreply.github.com>
2019-08-12 16:00:35 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 python manage.py migrate
-python manage.py collectstatic 
-python manage.py runserver 0.0.0.0:8000 --nothreading 
+#python manage.py collectstatic 
+python manage.py runserver 0.0.0.0:8000 
 # exec gunicorn learnDjango.wsgi:application \
 #      --name learnDjango \
 #      --bind 0.0.0.0:8000 \
